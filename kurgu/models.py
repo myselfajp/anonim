3796e2c89ba2a054b2e4c61457f0da1d120dda38
Ali Jalili Parsa <myjaliliparsa@gmail.com>
2023-03-28 16:25:49 +0300

    update

--- kurgu/models.py
+++ kurgu/models.py
@@ -30,10 +30,10 @@ class KJ(models.Model):
     title = models.CharField(default='', blank=True,max_length=250,verbose_name="Ekranda gözükecek unvan")
     instagram = models.CharField(default='', blank=True,max_length=50,verbose_name="İnstagram hesabı")
     website = models.CharField(default='', blank=True,max_length=250,verbose_name="Web adresi")
-    tel = models.CharField(default='', blank=True,max_length=20,verbose_name="Tel no")
+    tel = models.CharField(default='', blank=True,max_length=60,verbose_name="Tel no")
     sponser = models.CharField(default='', blank=True,max_length=250,verbose_name="ُSponsorluk")
     ad_banner = models.CharField(default='', blank=True,max_length=250,verbose_name="ُAlt bant reklam")
-    play_time_as_minuate = models.CharField(default='', blank=True,max_length=2,verbose_name="Yayın dakikasi")
+    play_time_as_minuate = models.CharField(default='', blank=True,max_length=60,verbose_name="Yayın dakikasi")
     note = models.TextField(blank=True,default='',verbose_name="Not")
 
     # Kurgu 
